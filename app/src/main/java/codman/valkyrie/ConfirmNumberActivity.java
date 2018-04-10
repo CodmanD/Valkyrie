8e12db8bcd41f58c87c45f0d5135f7e6e9f99276
CodmanD <korovin.dmitry.1977@gmail.com>
2018-04-10 19:21:39 +0300

    10.03/3

--- app/src/main/java/codman/valkyrie/ConfirmNumberActivity.java
+++ app/src/main/java/codman/valkyrie/ConfirmNumberActivity.java
@@ -27,7 +27,7 @@ public class ConfirmNumberActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_number);
         ButterKnife.bind(this);
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/codman/valkyrie/CreateOrderActivity.java
@@ -0,0 +1,10 @@
+package codman.valkyrie;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DI1 on 10.04.2018.
+ */
+
+public class CreateOrderActivity extends AppCompatActivity {
+}
